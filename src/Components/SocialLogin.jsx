26851ba661cbbe0662d44d1af2a8ba3ba47b9c25
AkashPaul923 <akashpaul923@gmail.com>
2025-02-20 23:27:16 +0600

    register page design

---
+++ src/Components/SocialLogin.jsx
@@ -0,0 +1,16 @@
+const SocialLogin = () => {
+    return (
+        <div className="my-3">
+            <button className="btn">
+                <img
+                    className="w-6"
+                    src="https://img.icons8.com/?size=100&id=17949&format=png&color=000000"
+                    alt=""
+                />
+                <p>Login with Google</p>
+            </button>
+        </div>
+    );
+};
+
+export default SocialLogin;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,129 @@
-
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import SocialLogin from "../Components/SocialLogin";
 
 const Register = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
-        <div>
-            
+        <div
+            className="flex items-center justify-center min-h-screen font-sans"
+        >
+            <div className=" rounded-lg p-8 max-w-xl flex w-11/12 border-2">
+                {/* Left Side: Login Form */}
+                <div className="w-full">
+                    <h2 className="text-2xl font-bold text-center mb-6">
+                        Register Account
+                    </h2>
+                    <form
+                        onSubmit={handleSubmit(onSubmit)}
+                        className="space-y-4"
+                    >
+                        {/* Name Input */}
+                        <div className="form-control w-full col-span-3">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                {...register("name", { required: true })}
+                                placeholder="Enter your name"
+                                className="input input-bordered bg-transparent   backdrop-blur-lg w-full"
+                            />
+                            {errors.name && (
+                                <span className="text-red-500">
+                                    This field is required
+                                </span>
+                            )}
+                        </div>
+
+                        {/* photo Input */}
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input
+                                type="URL"
+                                {...register("photo", { required: true })}
+                                className="input input-bordered bg-transparent backdrop-blur-lg w-full"
+                                placeholder="Enter your photo URL"
+                            />
+                            {errors.photo && (
+                                <span className="text-red-500">
+                                    This field is required
+                                </span>
+                            )}
+                        </div>
+
+                        {/* Email Input */}
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                {...register("email", { required: true })}
+                                placeholder="Enter your email"
+                                className="input input-bordered bg-transparent backdrop-blur-lg w-full"
+                            />
+                            {errors.name && (
+                                <span className="text-red-500">
+                                    This field is required
+                                </span>
+                            )}
+                        </div>
+
+                        {/* Password Input */}
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                {...register("password", {
+                                    required: true,
+                                    pattern:
+                                        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{6,}$/,
+                                })}
+                                placeholder="Enter your password"
+                                className="input input-bordered bg-transparent backdrop-blur-lg w-full"
+                            />
+                            {errors.password && (
+                                <span className="text-red-500">
+                                    Password must be an uppercase, a lowercase,
+                                    a number and minimum six character
+                                </span>
+                            )}
+                        </div>
+
+                        {/* Submit Button */}
+                        <input
+                            type="submit"
+                            className="btn btn-outline btn-primary w-full mt-4"
+                            value="Sign Up"
+                        />
+                    </form>
+
+                    {/* Additional Options */}
+                    <div className="text-center mt-4">
+                        <p className="text-sm ">
+                            Already have account?
+                            <Link to="/login" className="font-bold ml-2">
+                                Please login
+                            </Link>
+                        </p>
+                        <p className="text-sm mt-2">Or sign in with</p>
+                        <SocialLogin></SocialLogin>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
